914772a62f52b7931e304ea91203ef851d45492d
Giulia Casal <giuliacasal@Giulias-MacBook-Pro-2.local>
2023-08-08 21:46:47 +0100

    Refactored the solution to use a convert_binary_mask_input_file_to_tif method

--- mainNew.py
+++ mainNew.py
@@ -5,13 +5,14 @@ from PIL import Image
 import numpy as np
 
 
-def convert_encoded_binary_mask_to_image(mask):
+def convert_encoded_binary_mask_to_image(mask, first_image_masks, width, height, bbox):
+    # convert json file into binary mask and fit it into original image
     image_array = maskutil.decode(mask)
     first_image_masks.append(image_array)
     print(type(first_image_masks[0]))
     print(image_array.shape)
-    imagesize = mask_results["height"], mask_results["width"]
-    x0, y0, x1, y1 = obj["bbox"]
+    imagesize = height, width
+    x0, y0, x1, y1 = bbox
     w, h = x1 - x0, y1 - y0
     mask = np.zeros(imagesize)
     print(w, h)
@@ -19,18 +20,24 @@ def convert_encoded_binary_mask_to_image(mask):
     return mask
 
 
-with open('input/64ca5412fbb8640014c9fcc2_objects.json') as maskfile:
-    mask_results = json.load(maskfile)
-    first_image_objects_encoded = mask_results["objects"]
+def convert_binary_mask_input_file_to_tif(file_name):
+    with open(file_name) as maskfile:
+        mask_results = json.load(maskfile)
+        first_image_objects_encoded = mask_results["objects"]
 
-    first_image_masks = []
-    for obj in first_image_objects_encoded.values():
-        encoded_mask = obj["rle"]
-        encoded_mask["counts"] = base64.b64decode(encoded_mask["counts"])
+        first_image_masks = []
+        for obj in first_image_objects_encoded.values():
+            encoded_mask = obj["rle"]
+            encoded_mask["counts"] = base64.b64decode(encoded_mask["counts"])
 
-        # convert json file into binary mask and fit it into original image
-        image_mask = convert_encoded_binary_mask_to_image(encoded_mask)
-        im = Image.fromarray(image_mask)
-        im.save("output/binary_mask01.tif")
+            image_mask = convert_encoded_binary_mask_to_image(
+                encoded_mask, first_image_masks, mask_results["width"],
+                mask_results["height"], obj["bbox"]
+            )
+            im = Image.fromarray(image_mask)
+            im.save("output/binary_mask01.tif")
 
     print(f"Binary masks returned {len(first_image_masks)}")
+
+
+convert_binary_mask_input_file_to_tif('input/64ca5412fbb8640014c9fcc2_objects.json')

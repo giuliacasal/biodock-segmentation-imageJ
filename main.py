a3b354bef65804809070c7185774b797245bb209
Giulia Casal <giuliacasal@Giulias-MacBook-Pro-2.local>
2023-08-08 22:02:53 +0100

    Wrote the read_binary_mask_files_for_dir method

--- main.py
+++ main.py
@@ -1,55 +1,53 @@
 import base64
-import binascii
 import json
 import pycocotools.mask as maskutil
 from PIL import Image
 import numpy as np
-
-from polygon_scikit import binary_mask_to_polygons
-
-
-def print_hex(masks):
-    for mask in masks:
-        print(binascii.hexlify(bytearray(mask)))
+import os
 
 
-def save_fiji_format(polygons):
-    # Save the polygons to a FIJI X Y Coordinate file
-    with open("output/Coordinates.txt", 'w') as f:
-        f.write('X\tY\n')  # Write column headers
+def convert_encoded_binary_mask_to_image(mask, first_image_masks, width, height, bbox):
+    # convert json file into binary mask and fit it into original image
+    image_array = maskutil.decode(mask)
+    first_image_masks.append(image_array)
+    print(type(first_image_masks[0]))
+    print(image_array.shape)
+    imagesize = height, width
+    x0, y0, x1, y1 = bbox
+    w, h = x1 - x0, y1 - y0
+    mask = np.zeros(imagesize)
+    print(w, h)
+    mask[y0:y0 + h, x0:x0 + w] = image_array
+    return mask
 
-        for polygon in polygons:
-            for x, y in polygon:
-                f.write(f'{x}\t{y}\n')
 
+def convert_binary_mask_input_file_to_tif(file_name):
+    with open(file_name) as maskfile:
+        mask_results = json.load(maskfile)
+        first_image_objects_encoded = mask_results["objects"]
 
-with open('input/64ca5412fbb8640014c9fcc2_objects.json') as maskfile:
-    mask_results = json.load(maskfile)
-    first_image_objects_encoded = mask_results["objects"]
+        first_image_masks = []
+        for obj in first_image_objects_encoded.values():
+            encoded_mask = obj["rle"]
+            encoded_mask["counts"] = base64.b64decode(encoded_mask["counts"])
 
-    first_image_masks = []
-    for obj in first_image_objects_encoded.values():
-        encoded_mask = obj["rle"]
-        encoded_mask["counts"] = base64.b64decode(encoded_mask["counts"])
+            image_mask = convert_encoded_binary_mask_to_image(
+                encoded_mask, first_image_masks, mask_results["width"],
+                mask_results["height"], obj["bbox"])
 
-        image_array = maskutil.decode(encoded_mask)
-        first_image_masks.append(image_array)
-        print(type(first_image_masks[0]))
-        print(image_array.shape)
-        imagesize = mask_results["width"], mask_results["height"]
-        x0, y0, x1, y1 = obj["bbox"]
-        w, h = x1 - x0, y1 - y0
-        mask = np.zeros(imagesize)
-        print(w, h)
-        mask[y0:y0+h, x0:x0+w] = image_array
-
-        im = Image.fromarray(mask)
-        im.save("output/binary_mask.tif")
+            im = Image.fromarray(image_mask)
+            im.save("output/binary_mask01.tif")
 
     print(f"Binary masks returned {len(first_image_masks)}")
 
-    polygon_output = binary_mask_to_polygons(first_image_masks[0])
 
-    print(polygon_output)
+def read_binary_mask_files_for_dir(directory_path):
+    file_list = []
+    for filename in os.listdir(directory_path):
+        if os.path.isfile(os.path.join(directory_path, filename)):
+            file_list.append(filename)
+    return file_list
 
-    save_fiji_format(polygon_output)
+
+list1 = read_binary_mask_files_for_dir('input/J7784 BV2 10nm masks data/')
+print(len(list1))
